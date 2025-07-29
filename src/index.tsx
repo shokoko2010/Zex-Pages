f8cfeefa363a2f57505940717df50eaccefa836d
shokoko2010 <shokoko2010@gmail.com>
2025-07-29 18:14:11 +0000

    hgf

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css'; // Import global styles
+import '/index.css'; // Import global styles from root
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
